85ee3542f8fe98139a4659167732e646d3a385d4
masko-vl <58594257+masko-vl@users.noreply.github.com>
2020-05-12 17:30:46 +0200

    created ddbb and conection to server

---
+++ ddbb.sql
@@ -0,0 +1,34 @@
+CREATE TABLE `box_recipe` (
+  `id_box` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255),
+  `description` varchar(255)
+);
+CREATE TABLE `stock_ingredients` (
+  `id_stock` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255),
+  `expiration_date` date,
+  `kg` int
+);
+CREATE TABLE `ingredient` (
+  `id_ingredient` int PRIMARY KEY AUTO_INCREMENT, 
+  `name` varchar(255),
+  `alergies` varchar(255),
+  `id_stock` int
+);
+CREATE TABLE `quantities_ingredient` (
+  `id_ingredient` int,
+  `id_box` int,
+  `quantity` int
+);
+ALTER TABLE `quantities_ingredient` ADD FOREIGN KEY (`id_ingredient`) REFERENCES `ingredient` (`id_ingredient`);​
+ALTER TABLE `quantities_ingredient` ADD FOREIGN KEY (`id_box`) REFERENCES `box_recipe` (`id_box`);
+ALTER TABLE `ingredient` ADD FOREIGN KEY (`id_stock`) REFERENCES `stock_ingredients` (`id_stock`);
+
+
+
+INSERT INTO ingredient (id_ingredient, name, alergies, id_stock) VALUES(null,'Azucar', null, null);
+INSERT INTO ingredient (id_ingredient, name) VALUES(null,'Sal');
+INSERT INTO ingredient (id_ingredient, name, alergies, id_stock) VALUES(null,'Almidón Maiz', null, null);
+INSERT INTO ingredient (id_ingredient, name, alergies, id_stock) VALUES(null,'Levadura química', null, null);
+INSERT INTO ingredient (id_ingredient, name, alergies, id_stock) VALUES(null,'Goma Xantana', null, null);
+INSERT INTO ingredient (id_ingredient, name, alergies, id_stock) VALUES(null,'Canela en polvo', null, null);
---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express'),
+//información que tu le mandas por un formulario
+      bodyParser = require('body-parser'),
+      app = express(),
+      connection = require('./config'),
+      router = express.Router();
+
+
+const port = 5000;
+var cors = require('cors')
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+// 3 WE CONVERT INTO JSON FORMAT
+
+app.use(bodyParser.json());
+
+// ROUTE TO MAIN PAGE
+app.get('/', function(req, res) {
+    res.send('Start');
+});
+app.get('/ingredient', (req, res) => {
+    
+  connection.query('SELECT * FROM ingredient', (err, results) => {
+    if(err) {
+      res.status(500).send(err)
+    } else {
+      res.json(results)
+    }  })
+})
+
+app.listen(port,(err)=>{
+    if (err) {
+    throw new Error('Something bad happened...');
+    }
+    console.log(`Server listening to port ${port}`)
+});
