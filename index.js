6aa33d456c134f72bab7a8348d0d40a7b6d7ba7e
mireiaGomariz <mireiapgomariz@gmail.com>
2020-05-12 21:11:21 +0200

    add new routes for communicated tables

--- index.js
+++ index.js
@@ -30,6 +30,22 @@ app.get('/ingredient', (req, res) => {
     }  })
 })
 
+
+
+app.get('/stock_ingredients', (req, res) => {
+
+  connection.query('SELECT * FROM stock_ingredients', (err, results) => {
+    if(err) {
+      res.status(500).send(err)
+    } else {
+      res.json(results)
+    }  })
+})
+
+
+
+//Show all recipes
+
 app.get('/box_recipe', (req, res) => {
 
   connection.query('SELECT * FROM box_recipe', (err, results) => {
@@ -41,6 +57,83 @@ app.get('/box_recipe', (req, res) => {
 })
 
 
+//add post method to integrate recipe inside the table
+app.post('/box_recipe', (req, res) => {
+
+  const formData = {
+    name: req.body.name,
+    description: req.body.description,
+    ingredients_quantity: req.body.ingredients_quantity
+  }
+//put these information in the DB users table to create the new user
+    connection.query(`INSERT INTO box_recipe (name, description) VALUES ('${formData.name}', '${formData.description}')`
+ , (err) => {          // WE need to say formData to kw where to go searching the information
+      if(err){
+        res.status(500).send(err)
+      } else {
+        connection.query('SELECT id_box FROM box_recipe ORDER BY id_box DESC LIMIT 1', (err, results) => {
+          if(err) {
+            res.status(500).send(err)
+          } else {
+            let box_id = results[0].id_box
+
+
+          formData.ingredients_quantity.map((ingredient_quantity) => {
+            let ingredient = ingredient_quantity["ingredient"]
+            let quantity = ingredient_quantity["quantity"]
+
+              connection.query(`INSERT INTO quantities_ingredient (id_ingredient, id_box, quantity) VALUES (${ingredient}, ${box_id}, ${quantity}) `, (err) => {          // WE need to say formData to kw where to go searching the information
+                if(err){
+                  res.status(500).send(err)
+                } else {
+                  res.sendStatus(200)
+                }
+              })
+            })
+          }})
+        }
+      })
+  });
+
+// post ingredient
+
+app.post('/ingredient', (req, res) => {
+
+  const formData = {
+    name: req.body.name,
+    alergies: req.body.alergies,
+  }
+//put these information in the DB users table to create the new user
+    connection.query(`INSERT INTO ingredient SET ?`, formData, (err) => {          // WE need to say formData to kw where to go searching the information
+      if(err){
+        res.status(500).send(err)
+      } else {
+        console.log('BIG SUCCESS')
+        res.sendStatus(200)
+      }
+    })
+});
+
+
+// middle TABLE
+app.post('/quantities_ingredient', (req, res) => {
+
+  const formData = {
+    quantity: req.body.quantity,
+    id_ingredient: req.body.id_ingredient,
+    id_box: req.body.id_box
+  }
+
+
+//put these information in the DB users table to create the new user
+    connection.query(`INSERT INTO quantities_ingredient WHERE id_ingredient = ? AND id_box = ? SET quantity = ?`, formData, (err) => {          // WE need to say formData to kw where to go searching the information
+      if(err){
+        res.status(500).send(err)
+      } else {
+        res.sendStatus(200)
+      }
+    })
+});
 
 
 app.listen(port,(err)=>{
